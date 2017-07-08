3001b9ae72a292b6f9c6c16106bbc96d25a89b15
Arunesh <arunesh90@outlook.com>
2017-07-08 03:09:23 +0200

    I have no idea what this does

--- launcher/src/main/java/com/skcraft/launcher/swing/WebpagePanel.java
+++ launcher/src/main/java/com/skcraft/launcher/swing/WebpagePanel.java
@@ -253,6 +253,7 @@ public final class WebpagePanel extends JPanel {
                 conn.setDoInput(true);
                 conn.setDoOutput(false);
                 conn.setReadTimeout(5000);
+				conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 
                 conn.connect();
 
--- launcher/src/main/java/com/skcraft/launcher/util/HttpRequest.java
+++ launcher/src/main/java/com/skcraft/launcher/util/HttpRequest.java
@@ -110,7 +110,7 @@ public class HttpRequest implements Closeable, ProgressObservable {
             }
 
             conn = (HttpURLConnection) reformat(url).openConnection();
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Java) SKMCLauncher");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 
             if (body != null) {
                 conn.setRequestProperty("Content-Type", contentType);
